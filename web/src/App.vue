4fcd7a2082180faa8e72ac9db64db7322faadc51
DBKynd <db@dbkynd.com>
2023-12-05 20:27:45 -0700

    web: initial commit

---
+++ web/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <AppBar app />
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import AppBar from '@/components/AppBar.vue';
+</script>
+
+<style scoped></style>
---
+++ web/src/components/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title>Chat Moderation</v-app-bar-title>
+    <v-spacer />
+    <v-avatar size="48">
+      <v-img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+    </v-avatar>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
